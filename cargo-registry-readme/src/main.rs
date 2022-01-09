5cb1f319fb01416d6eb1c44f9df00c1ac2235007
rodzilla <rrrodzilla@protonmail.com>
2022-01-08 22:41:49 -0800

    Desktop version of crates.io mockup complete

--- cargo-registry-readme/src/main.rs
+++ cargo-registry-readme/src/main.rs
@@ -2,12 +2,8 @@ use anyhow::{bail, Result};
 use cargo_registry_readme_lib::preview;
 use clap::Parser;
 use std::fs;
-use std::io::Cursor;
 use std::net::TcpListener;
 use tiny_http::{Header, Response, Server};
-
-const CRATES_PATH: &str = "/crates";
-const LIBRS_PATH: &str = "/librs";
 
 #[derive(Parser, Debug)]
 #[clap(about, version, author)]
@@ -18,27 +14,6 @@ struct Args {
     /// 🌐 Automatically open the preview in your default browser
     #[clap(short, long)]
     open: bool,
-}
-
-fn default_response() -> Response<Cursor<Vec<u8>>> {
-    let preview_html = "<html><head><link rel='stylesheet' href='https://code.cdn.mozilla.net/fonts/fira.css'></head><body>Nothing here</body></html>";
-    let mut response = Response::from_string(preview_html);
-    response.add_header(Header::from_bytes(&b"Content-Type"[..], &b"text/html"[..]).unwrap());
-    response
-}
-
-fn librs_preview_response(readme: &str) -> Response<Cursor<Vec<u8>>> {
-    let preview_html = format!("<html><head><link rel='stylesheet' href='https://code.cdn.mozilla.net/fonts/fira.css'></head><body>{}</body></html>", preview(readme));
-    let mut response = Response::from_string(preview_html);
-    response.add_header(Header::from_bytes(&b"Content-Type"[..], &b"text/html"[..]).unwrap());
-    response
-}
-
-fn crates_preview_response(readme: &str) -> Response<Cursor<Vec<u8>>> {
-    let preview_html = format!("<html><head><link rel='stylesheet' href='https://code.cdn.mozilla.net/fonts/fira.css'></head><body>{}</body></html>", preview(readme));
-    let mut response = Response::from_string(preview_html);
-    response.add_header(Header::from_bytes(&b"Content-Type"[..], &b"text/html"[..]).unwrap());
-    response
 }
 
 fn main() -> Result<()> {
@@ -59,28 +34,50 @@ fn main() -> Result<()> {
 
     let server = Server::from_listener(listener, None).unwrap();
     println!("Preview readme 🦀🔎:");
-    println!("  📦crates.io: http://{}:{}{}", host, port, CRATES_PATH);
-    println!("  📦lib.rs: http://{}:{}{}", host, port, LIBRS_PATH);
+    println!("  📦crates.io: http://{}:{}", host, port);
 
     //open the default browser if the user chooses
     if args.open {
-        opener::open_browser(format!("http://{}:{}{}", host, port, CRATES_PATH))?;
-        opener::open_browser(format!("http://{}:{}{}", host, port, LIBRS_PATH))?;
+        opener::open_browser(format!("http://{}:{}", host, port))?;
     }
 
     for request in server.incoming_requests() {
-        //      println!(
-        //          "received request! method: {:?}, url: {:?}, headers: {:?}",
-        //          request.method(),
-        //          request.url(),
-        //          request.headers()
-        //      );
-        let response = match request.url() {
-            CRATES_PATH => crates_preview_response(&readme),
-            LIBRS_PATH => librs_preview_response(&readme),
-            _ => default_response(),
+        println!(
+            "received request! url: {:?}",
+            //           request.method(),
+            request.url(),
+            //            request.headers()
+        );
+        //handle the various requests
+        match request.url() {
+            //respond with the html page and inject the readme html
+            "/" => request.respond(
+                Response::from_string(
+                    include_str!("../../wireframes/crates/src/index.html")
+                        .replace("{}", &preview(&readme)),
+                )
+                .with_header("Content-Type: text/html".parse::<Header>().unwrap()),
+            )?,
+            //respond with the minified css file
+            "/output.css" => request.respond(
+                Response::from_data(
+                    include_bytes!("../../wireframes/crates/dist/output.css").as_ref(),
+                )
+                .with_header("Content-Type: text/css".parse::<Header>().unwrap()),
+            )?,
+            //respond with the logo file
+            "/logo.png" => request.respond(
+                Response::from_data(include_bytes!("../../wireframes/crates/logo.png").as_ref())
+                    .with_header("Content-Type: image/png".parse::<Header>().unwrap()),
+            )?,
+            //respond with the fira typography css file
+            "/fira.css" => request.respond(
+                Response::from_string(include_str!("../../wireframes/crates/src/fira.css"))
+                    .with_header("Content-Type: text/css".parse::<Header>().unwrap()),
+            )?,
+            //else respond with a 404
+            _ => request.respond(Response::empty(404))?,
         };
-        request.respond(response)?;
     }
 
     Ok(())
--- wireframes/crates/tailwind.config.js
+++ wireframes/crates/tailwind.config.js
@@ -1,12 +1,35 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-	    fontFamily: { sans: ['Fira',...defaultTheme.fontFamily.sans], }
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Fira Sans', ...defaultTheme.fontFamily.sans],
+                mono: ['Fira Mono', ...defaultTheme.fontFamily.mono],
+            },
+            maxWidth: {
+                '960': '960px',
+            },
+            height: {
+                '80': '80px',
+                '13': '3.35rem',
+            },
+            fontSize: {
+                'logo': '24px',
+            },
+            padding: {
+                '38': '9.5rem',
+            },
+            gridTemplateRows: {
+                'search-info': 'auto',
+            },
+            gridTemplateColumns: {
+                'crate-info': 'minmax(0,7fr) minmax(0,3fr)',
+                'search-info': 'auto 1fr auto',
+            },
+        },
     },
-  },
-  plugins: [
-      require('@tailwindcss/forms'),
-  ]
+    plugins: [
+        require('@tailwindcss/forms'),
+    ]
 }
