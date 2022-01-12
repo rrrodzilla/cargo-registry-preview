5d6433dfcae5534724c1d03250a07e8551d3d078
rodzilla <rrrodzilla@protonmail.com>
2022-01-12 05:45:30 -0800

    Update static file paths for publishing

--- cargo-registry-readme/src/main.rs
+++ cargo-registry-readme/src/main.rs
@@ -374,7 +374,7 @@ fn main() -> Result<()> {
             //respond with the html page and inject the readme html
             "/" => request.respond(
                 Response::from_string(
-                    include_str!("../../wireframes/crates/src/index.html")
+                    include_str!("/home/rodzilla/Documents/Projects/cargo-registry-preview/wireframes/crates/src/index.html")
                         .replace(
                             "{}",
                             &text_to_html(&fs::read_to_string(readme)?, "readme", None, None),
@@ -386,7 +386,7 @@ fn main() -> Result<()> {
             //respond with the minified highlights js file
             "/ws.js" => request.respond(
                 Response::from_string(
-                    include_str!("../../wireframes/crates/src/ws.js")
+                    include_str!("/home/rodzilla/Documents/Projects/cargo-registry-preview/wireframes/crates/src/ws.js")
                         .replace("{hot_reload_port}", &port.to_string())
                         .replace("{hot_reload_host}", hostname),
                 )
@@ -395,7 +395,7 @@ fn main() -> Result<()> {
             //respond with the minified highlights js file
             "/highlight.min.js" => request.respond(
                 Response::from_data(
-                    include_bytes!("../../wireframes/crates/src/highlight.min.js").as_ref(),
+                    include_bytes!("/home/rodzilla/Documents/Projects/cargo-registry-preview/wireframes/crates/src/highlight.min.js").as_ref(),
                 )
                 .with_header("Content-Type: text/javascript".parse::<Header>().unwrap()),
             )?,
@@ -403,14 +403,14 @@ fn main() -> Result<()> {
             //sourced from https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github.min.css
             "/github.min.css" => request.respond(
                 Response::from_data(
-                    include_bytes!("../../wireframes/crates/src/github.min.css").as_ref(),
+                    include_bytes!("/home/rodzilla/Documents/Projects/cargo-registry-preview/wireframes/crates/src/github.min.css").as_ref(),
                 )
                 .with_header("Content-Type: text/css".parse::<Header>().unwrap()),
             )?,
             //respond with the minified css file
             "/output.css" => request.respond(
                 Response::from_data(
-                    include_bytes!("../../wireframes/crates/dist/output.css").as_ref(),
+                    include_bytes!("/home/rodzilla/Documents/Projects/cargo-registry-preview/wireframes/crates/dist/output.css").as_ref(),
                 )
                 .with_header("Content-Type: text/css".parse::<Header>().unwrap()),
             )?,
@@ -418,7 +418,7 @@ fn main() -> Result<()> {
             //sourced from Rustacean.net
             "/rustacean-flat-happy.png" => request.respond(
                 Response::from_data(
-                    include_bytes!("../../wireframes/crates/rustacean-flat-happy.png").as_ref(),
+                    include_bytes!("/home/rodzilla/Documents/Projects/cargo-registry-preview/wireframes/crates/rustacean-flat-happy.png").as_ref(),
                 )
                 .with_header("Content-Type: image/png".parse::<Header>().unwrap()),
             )?,
@@ -426,54 +426,54 @@ fn main() -> Result<()> {
             //sourced from crates.io - rand crate (most downloaded all time)
             "/downloads.png" => request.respond(
                 Response::from_data(
-                    include_bytes!("../../wireframes/crates/download.png").as_ref(),
+                    include_bytes!("/home/rodzilla/Documents/Projects/cargo-registry-preview/wireframes/crates/download.png").as_ref(),
                 )
                 .with_header("Content-Type: image/png".parse::<Header>().unwrap()),
             )?,
             //respond with the crates.io favicon
             "/favicon.ico" => request.respond(
                 Response::from_data(
-                    include_bytes!("../../wireframes/crates/src/favicon.ico").as_ref(),
+                    include_bytes!("/home/rodzilla/Documents/Projects/cargo-registry-preview/wireframes/crates/src/favicon.ico").as_ref(),
                 )
                 .with_header("Content-Type: image/x-icon".parse::<Header>().unwrap()),
             )?,
             //respond with a required font
             "/woff/FiraSans-Bold.woff" => request.respond(
                 Response::from_data(
-                    include_bytes!("../../wireframes/crates/src/FiraSans-Bold.woff").as_ref(),
+                    include_bytes!("/home/rodzilla/Documents/Projects/cargo-registry-preview/wireframes/crates/src/FiraSans-Bold.woff").as_ref(),
                 )
                 .with_header("Content-Type: font/woff".parse::<Header>().unwrap()),
             )?,
             //respond with a required font
             "/woff/FiraSans-Italic.woff" => request.respond(
                 Response::from_data(
-                    include_bytes!("../../wireframes/crates/src/FiraSans-Italic.woff").as_ref(),
+                    include_bytes!("/home/rodzilla/Documents/Projects/cargo-registry-preview/wireframes/crates/src/FiraSans-Italic.woff").as_ref(),
                 )
                 .with_header("Content-Type: font/woff".parse::<Header>().unwrap()),
             )?,
             //respond with a required font
             "/woff/FiraSans-Regular.woff" => request.respond(
                 Response::from_data(
-                    include_bytes!("../../wireframes/crates/src/FiraSans-Regular.woff").as_ref(),
+                    include_bytes!("/home/rodzilla/Documents/Projects/cargo-registry-preview/wireframes/crates/src/FiraSans-Regular.woff").as_ref(),
                 )
                 .with_header("Content-Type: font/woff".parse::<Header>().unwrap()),
             )?,
             //respond with a required font
             "/woff/FiraMono-Regular.woff" => request.respond(
                 Response::from_data(
-                    include_bytes!("../../wireframes/crates/src/FiraMono-Regular.woff").as_ref(),
+                    include_bytes!("/home/rodzilla/Documents/Projects/cargo-registry-preview/wireframes/crates/src/FiraMono-Regular.woff").as_ref(),
                 )
                 .with_header("Content-Type: font/woff".parse::<Header>().unwrap()),
             )?,
             //respond with the logo file
             //sourced from crates.io
             "/logo.png" => request.respond(
-                Response::from_data(include_bytes!("../../wireframes/crates/logo.png").as_ref())
+                Response::from_data(include_bytes!("/home/rodzilla/Documents/Projects/cargo-registry-preview/wireframes/crates/logo.png").as_ref())
                     .with_header("Content-Type: image/png".parse::<Header>().unwrap()),
             )?,
             //respond with the fira typography css file
             "/fira.css" => request.respond(
-                Response::from_string(include_str!("../../wireframes/crates/src/fira.css"))
+                Response::from_string(include_str!("/home/rodzilla/Documents/Projects/cargo-registry-preview/wireframes/crates/src/fira.css"))
                     .with_header("Content-Type: text/css".parse::<Header>().unwrap()),
             )?,
             //else respond with a 308 to the index page
